20c62a35e6c5e9c80c08de998dbc5f83f965d851
Kschappacher <56745262+Kschappacher@users.noreply.github.com>
2024-10-25 14:38:26 -0400

    first commit

---
+++ sensor/cmd/remote/cmd.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"context"
+	"os"
+
+	"go.viam.com/rdk/module"
+
+	"go.viam.com/rdk/components/sensor"
+	"go.viam.com/rdk/config"
+	"go.viam.com/rdk/logging"
+	"go.viam.com/rdk/resource"
+	robotimpl "go.viam.com/rdk/robot/impl"
+	"go.viam.com/rdk/robot/web"
+	_ "go.viam.com/rdk/services/sensors/builtin"
+	"go.viam.com/utils"
+
+	maintenanceConfigSensor "github.com/kschappacher/maintenance-config-sensor/sensor"
+)
+
+func main() {
+    // NewLoggerFromArgs will create a logging.Logger at "DebugLevel" if
+    // "--log-level=debug" is an argument in os.Args and at "InfoLevel" otherwise.
+    utils.ContextualMain(mainWithArgs, module.NewLoggerFromArgs("testsensor"))
+}
+
+func mainWithArgs(ctx context.Context, args []string, logger logging.Logger) (err error) {
+
+    netconfig := config.NetworkConfig{}
+    netconfig.BindAddress = "0.0.0.0:8083"
+
+    if err := netconfig.Validate(""); err != nil {
+        return err
+    }
+
+	// Update the Attributes and ConvertedAttributes with the attributes your modular resource should receive
+    conf := &config.Config{
+        Network: netconfig,
+        Components: []resource.Config{
+            {
+                Name:  os.Args[1],
+                API:   sensor.API,
+                Model: maintenanceConfigSensor.Model,
+            },
+        },
+    }
+
+    myRobot, err := robotimpl.New(ctx, conf, logger)
+    if err != nil {
+        return err
+    }
+
+    return web.RunWebWithConfig(ctx, myRobot, conf, logger)
+}
